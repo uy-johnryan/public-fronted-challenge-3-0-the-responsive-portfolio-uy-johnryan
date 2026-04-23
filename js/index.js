bf7d88cf0408a8fc8d71160395b5d328e81b1d38
johnryanuy <20220025789@my.xu.edu.ph>
2026-04-23 09:26:37 +0800

    finalizing code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const links = document.querySelectorAll(".site-nav a");
 
-  /*nav links mixing with border color once clicked with only white text popping for emphasis*/
+  /*nav links mixing with border color once clicked with only white text popping*/
   links.forEach(link => {
     if (link.href === window.location.href) {
       link.style.background = "#4e387e";
